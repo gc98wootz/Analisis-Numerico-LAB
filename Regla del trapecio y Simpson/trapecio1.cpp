8349bfaa6350416cae921e6b081600a08be03889
Gonzalo Caira <gc98wootz@gmail.com>
2018-07-05 13:01:39 -0500

    Trapecio funcionando con simpson

--- Regla del trapecio y Simpson/trapecio1.cpp
+++ Regla del trapecio y Simpson/trapecio1.cpp
@@ -7,7 +7,8 @@ using namespace std;
 
 float funcion(float x)
 {
-return sin(x);
+//return sin(x);
+  return pow(x,4);
 }
 
 float trapecio(float a, float b, int n)
@@ -17,7 +18,7 @@ float trapecio(float a, float b, int n)
   for (int j=0;j<n;j++)
     sumatoria=sumatoria+funcion(a+j*h);
 
-  return (h/2)*(funcion(a)+2*sumatoria+funcion(b));
+  return (h/2)*(funcion(a)+2*sumatoria+funcion(b))*-1;
 }
 
 /*float trapecio_(float a, float b){
@@ -39,24 +40,25 @@ float Simpson1_3(float a,float b,int n){
      else
   	S1+=funcion(x);
    }
-   sumatoria=(h*(funcion(a)+4*S1+2*S0+funcion(b))/3.0);
+   sumatoria=(h*(funcion(a)+4*S1+2*S0+funcion(b))/3.0)*-1;
     return sumatoria;
 }
-float Simpson3_8(float a,float b,int n){
+float Simpson3_8(float a,float b,int n,float (*funcion)(float)){
   float sumatoria=0;
   float h;
-  float S0,S1;
+  float S0,S1,S2;
   h=(b-a)/n;
-  S0=S1=0;
-  for(int i=1;i<n-1;i++){
-    if(i%3)
-      S0+=2*funcion(a+i*h);
-    else
-      S1+=3*funcion(a+i*h);
+  S0=S1=S2=0;
+  for(int i=0;i<n/3;i++){
+    //if(i%3)
+      S0+=2*funcion(a+(i+2)*h);
+    //else
+      S1+=3*funcion(a+(i+1)*h);
+      S2+=3*funcion(a+i*h);
   }
   //cout<<S0<<endl;
   //cout<<S1<<endl;
-  sumatoria=(((3*h)/8)*(funcion(a)+S0+S1+funcion(b)));
+  sumatoria=(((3*h)/8)*(funcion(a)+S0+S1+S2+funcion(b)))*-1;
   //cout<<sumatoria<<endl;
   return sumatoria;
 }
@@ -64,8 +66,13 @@ float Simpson3_8(float a,float b,int n){
 int main(){
   //float (*f)(float);
   //f= funcion;
-  cout<<"Resultado TRAPECIO :\t" <<trapecio(pi,0,6)<<endl;
-  cout<<"Resultado Simpson 1/3 :\t"<<Simpson1_3(pi,0,6)<<endl;
-  cout<<"Resultado Simpson 3/8 :\t"<<Simpson3_8(pi,0,6)<<endl;
+  cout<<"\t N\t\ttrapecio\tsimpson1/3\tsimpson3/8"<<endl;
+  for(int i=6;i<36;i+=6){
+    cout<<"\t"<<i<<"\t\t"<<trapecio(1,0,i)<<"\t\t"<<Simpson1_3(1,0,i)<<"\t\t"<<Simpson3_8(1,0,i,funcion)<<endl;
+  }
+  /*cout<<"Resultado TRAPECIO :\t" <<trapecio(pi/2,0,96)<<endl;
+  cout<<"Resultado Simpson 1/3 :\t"<<Simpson1_3(pi/2,0,96)<<endl;
+  cout<<"Resultado Simpson 3/8 :\t"<<Simpson3_8(pi/2,0,96)<<endl;*/
+  //cout<<funcion(pi/2);
   return 0;
 }
